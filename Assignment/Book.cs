d4dfe694e66f255fae4128f1b359e21a5d38366c
Jyoti <thakurjyoti1430@gmail.com>
2025-02-02 22:38:53 +0530

    Assignment uploaded

---
+++ Assignment/Book.cs
@@ -0,0 +1,43 @@
+using System;
+
+class Book
+{
+    // Properties
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public double Price { get; set; }
+
+    // Default Constructor
+    public Book()
+    {
+        Title = "Introduction to Csharp";
+        Author = "Eric Gunnerson";
+        Price = 800;
+    }
+
+    // Parameterized Constructor
+    public Book(string title, string author, double price)
+    {
+        this.Title = title;
+        this.Author = author;
+        this.Price = price;
+    }
+
+    // Method to Display Book Details
+    public void Display()
+    {
+        Console.WriteLine("Title: " + Title + ", Author: " + Author + ", Price: $" + Price);
+    }
+
+    //  Main Method 
+    public static void Main()
+    {
+        // Creating object using Default Constructor
+        Book book1 = new Book();
+        book1.Display();
+
+        // Creating object using Parameterized Constructor
+        Book book2 = new Book("Clean Code", "Robert C. Martin", 1200);
+        book2.Display();
+    }
+}
